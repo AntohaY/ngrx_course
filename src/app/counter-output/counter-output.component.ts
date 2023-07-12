548c68e497a653ec84b2e90057d576ed9ee86a56
AntohaY <antonyakovenko2401@gmail.com>
2023-07-12 17:34:04 +0300

    first commit

---
+++ src/app/counter-output/counter-output.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css'],
+  standalone: true,
+  imports: [AsyncPipe]
+})
+export class CounterOutputComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{counter: number}>) {
+    this.count$ = store.select('counter');
+  }
+
+}
