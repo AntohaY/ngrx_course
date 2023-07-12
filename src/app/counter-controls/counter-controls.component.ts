6d4b4ee0c839269959804ff6328d1b90ab6b28e0
AntohaY <antonyakovenko2401@gmail.com>
2023-07-12 18:06:30 +0300

    feat: :zap: added decrememnt action and reducer

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.actions';
+import { increment, decrement } from '../store/counter.actions';
 
 
 @Component({
@@ -17,5 +17,6 @@ export class CounterControlsComponent {
   }
 
   decrement() {
+    this.store.dispatch(decrement({value: 1}));
   }
 }
